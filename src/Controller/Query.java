7a7fce1cbc4914b0393a09994bfb513b7d41ed2f
John Tang <jjt4623@rit.edu>
2018-03-03 05:13:03 -0500

    Adding comments and some basic functions.

--- src/Controller/Query.java
+++ src/Controller/Query.java
@@ -1,5 +1,16 @@
 package Controller;
 
-public interface Query {
-    void generateResponse();
+/**
+ * Encapsulates the query from the client.
+ * Command within the Command pattern
+ * Product within the Factory pattern
+ * Todo- create concrete Queries
+ */
+public interface Query
+{
+    /**
+     * Todo- Figure out what the command should produce
+     * @return A string that  represents the desired result.
+     */
+    public String generateResponse();
 }
