fa26c7432b94feebd43e3d820fe7751605c1658c
Calvin Do <cxd3796@g.rit.edu>
2018-03-04 18:59:50 -0500

    Adding Tests

---
+++ src/test/Model/AirportTest.java
@@ -0,0 +1,30 @@
+package test.Model;
+
+import com.flyboiz.afrs.Model.Airport;
+import org.junit.Before;
+import org.junit.Test;
+
+public class AirportTest
+{
+	private Airport CuT;
+	private String atlantaCode = "ATL";
+	private String sunnyCondition = "Sunny";
+	private int sunnyTemperature = 9001;
+	private int delayTime = 6969;
+
+
+	@Before
+	public void setup()
+	{
+		CuT = new Airport(atlantaCode);
+	}
+
+	@Test
+	public void storingAirportWeather()
+	{
+		CuT.storeAirportWeather(sunnyCondition, sunnyTemperature);
+		CuT.setDelayTime(delayTime);
+
+	}
+
+}
