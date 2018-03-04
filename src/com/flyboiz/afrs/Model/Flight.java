6cf38b6a2ae3773f1a517a7b725e78e76c75665e
Kent Brown <randall.k.brown.jr@gmail.com>
2018-03-03 20:01:45 -0500

    Kent Brown: - I'm not entirely sure what I accomplished.

--- src/com/flyboiz/afrs/Model/Flight.java
+++ src/com/flyboiz/afrs/Model/Flight.java
@@ -1,5 +1,40 @@
+/* Authors
+Kent Brown
+ */
+
+/* Package */
 package com.flyboiz.afrs.Model;
 
+/* Imports */
+
+/* Implementation */
 public class Flight
 {
+    // State //
+    private String originAirport;
+    private String destinationAirport;
+    private Time departureTime;
+    private Time arrivalTime;
+    private int flightNumber;
+    private int airfare;
+
+    // Constructors //
+    public Flight (String originAirport,
+                   String destinationAirport,
+                   Time departureTime,
+                   Time arrivalTime,
+                   int flightNumber,
+                   int airfare) {
+        this.originAirport = originAirport;
+        this.destinationAirport = destinationAirport;
+        this.departureTime = departureTime;
+        this.arrivalTime = arrivalTime;
+        this.flightNumber = flightNumber;
+        this.airfare = airfare;
+    }
+
+    // Behaviour //
+    public int getFlightNumber() {
+        return flightNumber;
+    }
 }
