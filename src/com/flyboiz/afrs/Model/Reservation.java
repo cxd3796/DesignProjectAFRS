849f23a65dfe44d665ae307dc63bb336db5dfcd0
Calvin Do <cxd3796@g.rit.edu>
2018-03-03 19:56:11 -0500

    Reservation and Reservation Database

--- src/com/flyboiz/afrs/Model/Reservation.java
+++ src/com/flyboiz/afrs/Model/Reservation.java
@@ -2,4 +2,14 @@ package com.flyboiz.afrs.Model;
 
 public class Reservation
 {
+	private Itinerary itinerary;
+	private String passengerName;
+
+	public Reservation(Itinerary itinerary, String passengerName)
+	{
+		this.itinerary = itinerary;
+		this.passengerName = passengerName;
+	}
+
+
 }
