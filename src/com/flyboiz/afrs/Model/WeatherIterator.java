4be2fe157dcc6bd3b105fbe6e94be7aa87a683a6
Trevor Lezynski <tlezynski@gmail.com>
2018-03-31 13:31:22 -0400

    Added list functionality to teh iterator

--- src/com/flyboiz/afrs/Model/WeatherIterator.java
+++ src/com/flyboiz/afrs/Model/WeatherIterator.java
@@ -1,5 +1,7 @@
 package com.flyboiz.afrs.Model;
 
+import java.util.List;
+
 /**
  * designproject-flyboiz
  * Created by Trevor Lezynski on 3/31/2018.
@@ -8,21 +10,27 @@ public class WeatherIterator {
 
     private int currentPosition;
     private final int MAX_SIZE;
+    private List list;
 
-    public WeatherIterator(int maxSize){
+    public WeatherIterator(List list){
         currentPosition = 0;
-        MAX_SIZE = maxSize;
+        MAX_SIZE = list.size();
+        this.list = list;
     }
 
-    public void next(){
+    public Object next(){
+        Object obj;
         if(currentPosition != MAX_SIZE) {
+            obj = list.get(getCurrentIndex());
             currentPosition++;
         } else {
+            obj = list.get(getCurrentIndex());
             currentPosition = 0;
         }
+        return obj;
     }
 
-    public int currentIndex(){
+    public int getCurrentIndex(){
         return currentPosition;
     }
 
