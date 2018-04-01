95b435d20aa544ab31aca341da8dd55587e27eba
Joanna Ye <jxy1164@rit.edu>
2018-04-01 15:32:10 -0400

    finished all concrete factories. added database references to queryServer

--- src/com/flyboiz/afrs/Controller/QueryCreators/QRetrieveCreator.java
+++ src/com/flyboiz/afrs/Controller/QueryCreators/QRetrieveCreator.java
@@ -1,4 +1,49 @@
 package com.flyboiz.afrs.Controller.QueryCreators;
+import com.flyboiz.afrs.Controller.Commands.QueryRetrieve;
+import com.flyboiz.afrs.Model.AirportDatabase;
+import com.flyboiz.afrs.Model.ClientDatabase;
+import com.flyboiz.afrs.Model.ReservationDatabase;
 
-public class QRetrieveCreator {
+public class QRetrieveCreator extends QueryCreator {
+    private ReservationDatabase reservationDB;
+    private AirportDatabase airportDB;
+    private ClientDatabase clientDB;
+
+    /**
+     * constructor
+     * @param clientDB client database
+     * @param reservationDB reservation database
+     * @param airportDB airport database
+     */
+    public QRetrieveCreator(ClientDatabase clientDB, ReservationDatabase reservationDB,
+                            AirportDatabase airportDB){
+        this.clientDB = clientDB;
+        this.reservationDB = reservationDB;
+        this.airportDB = airportDB;
+    }
+
+    /**
+     * makes a QueryRetrieve from user input;
+     *input should be in format retrieve, name, [origin],[destination]
+     * @param input
+     * @return
+     */
+    public QueryRetrieve makeQuery(String input){
+        String[] split = input.split(",");
+        if(split.length<3 || split.length>5){
+            return null;
+        }
+        int cid = Integer.parseInt(split[0]);
+        String name = split[2];
+        String origin = "";
+        String destination = "";
+        if(split.length>3){
+            origin = split[3];
+        }
+        if(split.length==5){
+            destination = split[4];
+        }
+        return new QueryRetrieve(cid, name, origin, destination, reservationDB, airportDB);
+    }
+
 }
